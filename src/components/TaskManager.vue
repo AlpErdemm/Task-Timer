b62c7cbea1297a446ed86e082fea6d3c28890a9a
AlpErdem <alp.erdem@reengen.com>
2022-05-29 22:56:41 +0300

    Add initial Task Manager

--- src/components/TaskManager.vue
+++ src/components/TaskManager.vue
@@ -1,9 +1,42 @@
-<script setup>
+<script>
+let id = 0
+export default{
+    data(){
+        return{
+            newTodo : '',
+            todos : [
+                        {id: id++, done:true, text:"This is task!"},
+                        {id: id++, done:false, text:"Looks like there is more??"},
+                    ]
+            }
+    },
+    methods : {
+        addTodo(){
+            console.log("Add!")
+            this.todos.push({ id: id++, done:false, text: this.newTodo })
+            this.newTodo = ''
+        },
+        removeTodo(todo){
+            console.log("Remove!")
+            this.todos = this.todos.filter((t) => t !== todo)
+        }
+    }
+}
 </script>
 
 <template>
   <div class="TaskManager">
-      <h1 class="taskManagerPlaceholder">Task Manager Here!</h1>
+      <h1 class="taskManagerPlaceholder">Tasks</h1>
+      <form @submit.prevent="addTodo">
+            <input v-model="newTodo">
+            <button>Add Task</button>    
+        </form>
+      <ul>
+        <li class="listElem" v-for="todo in todos" :key="todo.id">
+            {{todo.text}}
+            <button v-on:click="removeTodo(todo)">X</button>
+        </li>
+       </ul>
   </div>
 </template>
 
@@ -11,6 +44,15 @@
 .taskManagerPlaceholder{
     color: white;
     text-align: center;
+}
+.listElem{
+    color: white;
+    text-align: center;
+}
+form
+{
+    display:flex;
+    justify-content: center;
 }
 
 </style>
