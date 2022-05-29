11df8e802d6e06aecc7778b151182c5c9ab37ee0
AlpErdem <alp.erdem@reengen.com>
2022-05-29 20:08:43 +0300

    Add general layout

---
+++ src/components/TaskManager.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="TaskManager">
+      <h1 class="taskManagerPlaceholder">Task Manager Here!</h1>
+  </div>
+</template>
+
+<style>
+.taskManagerPlaceholder{
+    color: white;
+    text-align: center;
+}
+
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="Timer">
+      <h1 class="timerPlaceholder">Timer Here!</h1>
+  </div>
+</template>
+
+<style>
+.timerPlaceholder{
+    color: white;
+    text-align: center;
+}
+
+</style>
